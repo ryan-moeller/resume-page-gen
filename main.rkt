67e6abd8c6124dca443457fdd4e5812151450676
Ryan Moeller <ryan-moeller@att.net>
2024-04-06 17:15:04 -0400

    Update copyright year

--- main.rkt
+++ main.rkt
@@ -2,7 +2,7 @@
 
 ;; ISC License
 ;;
-;; Copyright (c) 2018, Ryan Moeller
+;; Copyright (c) 2018-2024, Ryan Moeller
 ;;
 ;; Permission to use, copy, modify, and/or distribute this software for any
 ;; purpose with or without fee is hereby granted, provided that the above
